fe72233f7c6cd165bb91e68ba1c9b7e1edbeb8ed
Eden Syoum <eden@temple.edu>
2016-06-27 19:58:01 -0400

    added window onload and loop that will iterate through divs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("I've been linked!");
+window.onload = function () {
+  console.log("I've been linked!");
+
+
+ var boxes = document.getElementsByClassName('box');
+    for (var i=0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', start);
+}
+
+// need to create "play" function here
+
+
+}
