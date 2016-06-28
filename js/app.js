fc6365613ad8c29bab3184b32ca9b356ffbfca7c
Eden Syoum <eden@temple.edu>
2016-06-28 10:33:45 -0400

    starting JS flow

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ window.onload = function () {
   console.log("I've been linked!");
 
 
- var boxes = document.getElementsByClassName('box');
+ var boxes = document.getElementById('box');
     for (var i=0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', start);
+    boxes[i].addEventListener('click', playGame);
 }
+
+var firstPlayer = 'X';
 
 // need to create "play" function here
 
+  var playGame = function () {
 
+
+  }
 }
