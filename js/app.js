2c6663e46d70eb193b02712cca7d81a7dc4802e2
Eden Syoum <eden@temple.edu>
2016-06-28 16:40:09 -0400

    committing slight additions

--- js/app.js
+++ js/app.js
@@ -2,17 +2,28 @@ window.onload = function () {
   console.log("I've been linked!");
 
 
- var boxes = document.getElementById('box');
+ var boxes = document.getElementsByClassName('box');
+ console.log(boxes); // it sees the div boxes
     for (var i=0; i < boxes.length; i++) {
     boxes[i].addEventListener('click', playGame);
 }
 
-var firstPlayer = 'X';
+// defining variables
 
-// need to create "play" function here
+var playerX = 'X';
+var playerO = 'O';
+var turn = 0; // to switch turns between X and O
+
+// need to create "playGame" function here
 
   var playGame = function () {
+    var turn = "X";
+    whosTurn(this.turn + "should begin the game.");
 
 
   }
-}
+
+  function whosTurn(turnMsg) { // idea for the message that will
+    document.getElementById("messageArea").innerHTML = turnMsg;
+  }
+};
