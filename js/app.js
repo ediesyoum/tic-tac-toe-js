7c92883d8a0ca18b2ee155063d46aab542c0225d
Eden Syoum <eden@temple.edu>
2016-06-28 21:17:05 -0400

    Almost forgot to commit after all a ton of struggles with JS today - got Xs and Os to switch and appear WEE

--- js/app.js
+++ js/app.js
@@ -1,29 +1,50 @@
 window.onload = function () {
   console.log("I've been linked!");
 
+  var startButton = document.getElementById('start');
+  startButton.onclick = function() {
+    startGame();
+  }
+
+
 
  var boxes = document.getElementsByClassName('box');
- console.log(boxes); // it sees the div boxes
+ console.log(boxes); // it sees the div boxes wee
+
+var startGame = function () {
     for (var i=0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', playGame);
+      boxes[i].addEventListener('click', function() {
+      playerTurn(this)
+    })
+  }
 }
-
-// defining variables
-
-var playerX = 'X';
-var playerO = 'O';
-var turn = 0; // to switch turns between X and O
 
 // need to create "playGame" function here
 
-  var playGame = function () {
-    var turn = "X";
-    whosTurn(this.turn + "should begin the game.");
+  // var playGame = function () {
+  //   console.log('s')
+  //
+  //   whosTurn(currentPlayer + " should begin the game.");
+  // }
 
+var currentPlayer = 1;
 
+  var playerTurn = function(box){
+    console.log(box);
+      if (!box.innerHTML) { // help from Reed Daniel
+        if (currentPlayer === 0) {
+          box.innerHTML = 'O';
+          currentPlayer = 1;
+        } else {
+          box.innerHTML = 'X';
+          currentPlayer = 0;
+        }
+      } else {
+      alert("You've been clicked!");
+    }
   }
 
-  function whosTurn(turnMsg) { // idea for the message that will
+  function whosTurn(turnMsg) { // idea for the message that will tell the user whos turn it is
     document.getElementById("messageArea").innerHTML = turnMsg;
   }
 };
