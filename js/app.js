7709a13d8a865dff6c8c11d4282c1f93f01f461c
Eden Syoum <eden@temple.edu>
2016-06-30 08:29:37 -0400

    more work on js flow and trying to implement a checkforwin

--- js/app.js
+++ js/app.js
@@ -20,16 +20,12 @@ var playerO = [];
  var boxes = document.getElementsByClassName('box');
  var p1Name = document.getElementById('p1namefield').value; // grabbing what is in the input field
  var p2Name = document.getElementById('p2namefield').value;
- console.log(p1Name)
- console.log(p2Name)
 
-
- console.log(boxes); // it sees the div boxes wee
+ // console.log(boxes); it sees the div boxes wee
 
 var startGame = function () {
     for (var i=0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
-
       playGame(this);
       //this.removeEventListener('click', function(){});
       whosTurn(p1Name + " has taken their turn." + p2Name + " should now take their turn." ); // this message is changing with the user WEE
@@ -37,19 +33,13 @@ var startGame = function () {
   }
 }
 
-var winningCombos = [ //combinations by ID
- [1, 2, 3] , [4, 5, 6] , [7, 8, 9], //horizontal rows
- [1, 4, 7], [2, 5, 8], [3, 6, 9], //vertical columns
- [1, 5, 9], [3, 5, 7] //diagonals
-]
-
-
+var whosTurn = function (turnMsg) { // idea for the message that will tell the user whos turn it is
+  document.getElementById("messageArea").innerHTML = turnMsg;
+}
 
 // need to create "playGame" function here
-
   // var playGame = function () {
   //   console.log('s')
-  //
   //   whosTurn(currentPlayer + " should begin the game.");
   // }
 
@@ -69,12 +59,34 @@ var currentPlayer = 1;
           console.log(playerX);
           currentPlayer = 0;
         }
-    } //else {
-       //alert("You've been clicked!");  // alerts are annoying !
-     //}
+    } else {
+     checkForWins();
   }
 
-  function whosTurn(turnMsg) { // idea for the message that will tell the user whos turn it is
-    document.getElementById("messageArea").innerHTML = turnMsg;
+// not sure how to make this work only after the first player has had 3 turns
+var checkForWins = function (box) {
+  whosTheWinner();
+}
+
+// combinations by ID - shortest way to write this possible
+var winningCombos = [
+   [1, 2, 3] , [4, 5, 6] , [7, 8, 9], //horizontal rows
+   [1, 4, 7], [2, 5, 8], [3, 6, 9], //vertical columns
+   [1, 5, 9], [3, 5, 7] //diagonals
+];
+
+  var whosTheWinner = function () {
+    if (winningCombos(playerX === 'X')) {
+      for (var i=0; i < playerX.length; i++) {
+        messageArea.innerHTML="X is the winner";
+      }
+    } else {
+      if (winningCombos(playerO === 'O')) {
+        for (var j=0; j < playerO.length; j++) {
+          messageArea.innerHTML="O is the winner";
+      }
+    }
+  }
+  }
   }
 };
