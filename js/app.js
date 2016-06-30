87a30c007e24b5cc7715dc988a42774b3941092a
Eden Syoum <eden@temple.edu>
2016-06-30 07:18:58 -0400

    got X and O arrays working correctly

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ window.onload = function () {
   startButton.onclick = function() {
     var p1Name = document.getElementById('p1namefield').value;
     var p2Name = document.getElementById('p2namefield').value;
-    document.getElementById('gameboard').style.display = '';
+    document.getElementById('gameboard').style.display = ''; // help from Derek to get the tiles to show only after names are entered
     document.getElementById('name-entry').style.display = 'none';
     document.getElementById("messageArea").innerHTML = "Player X will be " + p1Name + " , Player O will be " + p2Name;
     startGame();
@@ -14,18 +14,23 @@ window.onload = function () {
 //on page load, gameboard should be display none, when start button is clicked
 //gameboard should be display= '' and section with will be display none
 
-// attempting to make the variables below gl
+// attempting to make the variables below global
+var playerX = []; // must tell all clicks from this player to go into this array
+var playerO = [];
  var boxes = document.getElementsByClassName('box');
  var p1Name = document.getElementById('p1namefield').value; // grabbing what is in the input field
  var p2Name = document.getElementById('p2namefield').value;
+ console.log(p1Name)
+ console.log(p2Name)
 
- // console.log(boxes); // it sees the div boxes wee
+
+ console.log(boxes); // it sees the div boxes wee
 
 var startGame = function () {
     for (var i=0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
 
-      playerTurn(this);
+      playGame(this);
       //this.removeEventListener('click', function(){});
       whosTurn(p1Name + " has taken their turn." + p2Name + " should now take their turn." ); // this message is changing with the user WEE
     })
@@ -43,19 +48,23 @@ var startGame = function () {
 
 var currentPlayer = 1;
 
-  var playerTurn = function(box){
+  var playGame = function(box){
     console.log(box);
       if (!box.innerHTML) { // help from Reed Daniel & Derek
         if (currentPlayer === 0) {
           box.innerHTML = 'O';
+          playerO.push(box.id); // pushes the ID of the box selected in to the playerO array
+            console.log(playerO);
           currentPlayer = 1;
         } else {
           box.innerHTML = 'X';
+          playerX.push(box.id); // pushes the ID of the box selected in to the playerX array
+          console.log(playerX);
           currentPlayer = 0;
         }
-    }// else {
-    //   alert("You've been clicked!");
-    // }
+    } //else {
+       //alert("You've been clicked!");  // alerts are annoying !
+     //}
   }
 
   function whosTurn(turnMsg) { // idea for the message that will tell the user whos turn it is
