aa16d110318895544ed61b90753e81def0e019bf
Eden Syoum <eden@temple.edu>
2016-08-30 15:37:15 -0400

    fixing merge issues

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+<<<<<<< 7eaf7d76677ef158e4b9913890b8618e1ddd9a25
   function whosTurn (turnMsg) { // is
     document.getElementById("messageArea").innerHTML = turnMsg;
   }
@@ -88,4 +89,66 @@ function checkForWins (boxes) { // passing through boxes as a param
             alert("It's a tie - no one wins.");
             }
       };
+=======
+  console.log("I've been linked!");
+
+  var startButton = document.getElementById('start');
+  startButton.onclick = function() {
+    startGame();
+  }
+
+  var p1Button = document.getElementById('p1submit');
+  p1Button.addEventListener('click', function() {
+
+  })
+
+  var p2Button = document.getElementById('p2submit');
+  p2Button.addEventListener('click', function() {
+  //  player 2 div inner html equals the name in the input
+  //  input value of the input field that has player 2 id
+
+  })
+
+
+ var boxes = document.getElementsByClassName('box');
+ console.log(boxes); // it sees the div boxes wee
+
+var startGame = function () {
+    for (var i=0; i < boxes.length; i++) {
+      boxes[i].addEventListener('click', function() {
+      playerTurn(this)
+    })
+  }
+}
+
+
+// need to create "playGame" function here
+
+  // var playGame = function () {
+  //   console.log('s')
+  //
+  //   whosTurn(currentPlayer + " should begin the game.");
+  // }
+
+var currentPlayer = 1;
+
+  var playerTurn = function(box){
+    console.log(box);
+      if (!box.innerHTML) { // help from Reed Daniel & Derek
+        if (currentPlayer === 0) {
+          box.innerHTML = 'O';
+          currentPlayer = 1;
+        } else {
+          box.innerHTML = 'X';
+          currentPlayer = 0;
+        }
+      } else {
+      alert("You've been clicked!");
+    }
+  }
+
+  // function whosTurn(turnMsg) { // idea for the message that will tell the user whos turn it is
+    document.getElementById("messageArea").innerHTML = turnMsg;
+  // }
+>>>>>>> first pages commit
 };
